0957a0342e8a4ec1b2c8411da86af77289551135
alex tritetskyi <alextritecky@gmail.com>
2023-07-14 13:53:47 +0300

    update build

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
+
 const app = express();
-app.use(express.static(__dirname + "/dist/"));
+app.use(cors());
+app.use(express.static(__dirname + "/dist/frontend"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/frontend/index.html"));
 });
 app.listen(process.env.PORT || 8080);
