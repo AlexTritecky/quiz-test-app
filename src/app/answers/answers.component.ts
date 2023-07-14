f1c4b0f5c5961295967b70952b1772638ba859d9
alex tritetskyi <alextritecky@gmail.com>
2023-07-14 10:39:40 +0300

    add minor change

--- src/app/answers/answers.component.ts
+++ src/app/answers/answers.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 import { Results } from "../models/results.interface";
 
 @Component({
   selector: "app-answers",
   templateUrl: "./answers.component.html",
   styleUrls: ["./answers.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AnswersComponent {
   @Input()
